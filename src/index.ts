488c6b41036c64e18f65e3be61d88205aa2f451b
emertola <emertola@gmail.com>
2024-07-24 18:01:58 +0800

    chore: remove cors origin

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT ?? 3001;
 
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
 
